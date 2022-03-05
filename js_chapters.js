b2cb916749a045982147e3d81e40639158ba618a
Samuele Mattiuzzo <samumatt@gmail.com>
2022-03-05 23:35:25 +0000

    Improvement, some game logic

---
+++ js_chapters.js
@@ -0,0 +1,72 @@
+let chapters = {
+  1: {
+    1 : {
+      "text": "chapter 1 - 1 text",
+      "default": 1,
+      "options": {
+        1: {
+          "text": "option 1",
+          "modifiers": {
+            "oxygen": 0, "sanity": 0, "morale": 0, "panic": 0
+          },
+          "go_to": [1, 3]
+        },
+        2: {
+          "text": "option 2",
+          "modifiers": {
+            "oxygen": 0, "sanity": 0, "morale": 0, "panic": 0
+          },
+          "go_to": [1, 1]
+        },
+        3: {
+          "text": "option 3",
+          "modifiers": {
+            "oxygen": 0, "sanity": 0, "morale": 0, "panic": 0
+          },
+          "go_to": [2, 2]
+        },
+        4: {
+          "text": "option 4",
+          "modifiers": {
+            "oxygen": 0, "sanity": 0, "morale": 0, "panic": 0
+          },
+          "go_to": [1, 1]
+        }
+      }
+    },
+    2 : {
+      "text": "chapter 1 - 2 text",
+      "default": 1,
+      "options": {
+        1: {
+          "text": "option 1",
+          "modifiers": {
+            "oxygen": 0, "sanity": 0, "morale": 0, "panic": 0
+          },
+          "go_to": [1, 1]
+        },
+        2: {
+          "text": "option 2",
+          "modifiers": {
+            "oxygen": 0, "sanity": 0, "morale": 0, "panic": 0
+          },
+          "go_to": [1, 1]
+        },
+        3: {
+          "text": "option 3",
+          "modifiers": {
+            "oxygen": 0, "sanity": 0, "morale": 0, "panic": 0
+          },
+          "go_to": [1, 1]
+        },
+        4: {
+          "text": "option 4",
+          "modifiers": {
+            "oxygen": 0, "sanity": 0, "morale": 0, "panic": 0
+          },
+          "go_to": [1, 1]
+        }
+      }
+    },
+  }
+}
--- js_game.js
+++ js_game.js
@@ -1,18 +1,77 @@
 window.addEventListener("load", function(){
+  // Game defaults
   let intervalTimer = 20 * 1000; // 20 seconds
-  let text = ["Welcome", "Hi", "Sup dude"];
-  let counter = 0;
-  let elem = document.getElementById("text_box_text");
+
+  // Stats decay time and modifiers
+  // Modifiers change based on the player's choices
+  let oxygen_decay = -2;
+  let sanity_decay = -1;
+  let morale_decay = -2;
+  let panic_decay = 4;
+  let oxygen_modifier = 0;
+  let sanity_modifier = 0;
+  let morale_modifier = 0;
+  let panic_modifier = 0;
+
+  // UI elements
+  let text_box_text = document.getElementById("text_box_text");
+  let chapter_val = document.getElementById("chapter_val");
+  let route_val = document.getElementById("route_val");
+
+  let option_1 = document.getElementById("option_1");
+  let option_2 = document.getElementById("option_2");
+  let option_3 = document.getElementById("option_3");
+  let option_4 = document.getElementById("option_4");
+
+  let oxygen = document.getElementById("oxygen_val");
+  let sanity = document.getElementById("sanity_val");
+  let morale = document.getElementById("morale_val");
+  let panic = document.getElementById("panic_val");
+
+  // Game variables
+  let is_playing = false;
+  let current_chapter = 1;
+  let current_route = 1;
+  let current_choice = 0;
+  let next_route = [1, 1];
   
-  let inst = setInterval(change, intervalTimer);
+  //let inst = setInterval(change, intervalTimer);
 
-  function change() {
-    elem.innerHTML = text[counter];
-    counter++;
-    if (counter >= text.length) {
-      counter = 0;
-      // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  function load_next() {
+    current_chapter = next_route[0];
+    current_route = next_route[1];
+    update();
+  }
 
-    }
+  function resolve() {
+    // gets the modifier based on the player's choice, updates them accordingly
+    let modifiers = chapters[current_chapter][current_route].options[current_choice];
+    let next_route = chapters[current_chapter][current_route].options.go_to;
+    console.log(modifiers);
+  }
+
+  function update() {
+    // gets the current chapter
+    let current_obj = chapters[current_chapter][current_route];
+
+    // set the defaults, so that if the player has not responded
+    // when the timer expires, we make a choice for them (evil!)
+    current_choice = chapters[current_chapter][current_route].default;
+    next_route = chapters[current_chapter][current_route].options[current_choice].go_to;
+
+    // update UI steps
+    text_box_text.innerHTML = current_obj.text;
+    chapter_val.innerHTML = current_chapter;
+    route_val.innerHtml = current_route;
+    option_1.innerHTML = current_obj.options[1];
+    option_2.innerHTML = current_obj.options[2];
+    option_3.innerHTML = current_obj.options[3];
+    option_4.innerHTML = current_obj.options[4];
+  }
+
+  function startGame() {
+    update();
   };
+
+  update();
 });
